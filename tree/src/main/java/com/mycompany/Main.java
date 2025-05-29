6f56823ea1919ab791a7bf89ef122cd4acdd0689
Natalio Feliciano Solis <nsolis_240000000446@uic.edu.ph>
2025-05-29 15:41:17 +0800

    Add node insertion and inorder traversal demonstration to Main.java

--- tree/src/main/java/com/mycompany/Main.java
+++ tree/src/main/java/com/mycompany/Main.java
@@ -1,8 +1,26 @@
 package com.mycompany;
+
+// Import the BinaryTree class
+import com.mycompany.BinaryTree;
 import java.util.*;
 
+// Main class to demonstrate BinaryTree usage
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a new BinaryTree instance
+        BinaryTree tree = new BinaryTree();
+
+        // Insert nodes into the binary tree
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // Print the inorder traversal of the tree
+        System.out.println("Inorder traversal:");
+        tree.inorder();
     }
 }
